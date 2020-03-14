ba910cace71e587e085cffe958d63ad21d38310d
engineerbaz <engineerbaz@gmail.com>
2020-03-14 19:56:13 +0500

    update all 55.tt

--- gitBatch.sh
+++ gitBatch.sh
@@ -1,7 +1,8 @@
+echo "****** Welcome to Git *****" 
 ls
 git status
 read fname
 git add $fname
-git commit -m "\$fname"
+git commit -m "update on file $fname"
 git push origin master
-
+echo "--------- Completed update on Git ------"
