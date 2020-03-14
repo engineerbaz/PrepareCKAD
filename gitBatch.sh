568ff3eb9b409ef46fdf1197e1555432a3324587
engineerbaz <engineerbaz@gmail.com>
2020-03-14 20:03:04 +0500

    update on file gitBatch.sh

--- gitBatch.sh
+++ gitBatch.sh
@@ -1,8 +1,18 @@
-echo "****** Welcome to Git *****" 
+echo "                           " 
+echo "****** Welcome to Git Script by BAZ *****" 
+echo "              "
+echo "This script will upadte file to GtHub by taking filename input"
+
 ls
 git status
+
+echo "Please enter file name you want to upload to remote repo? "
+echo " "
 read fname
 git add $fname
 git commit -m "update on file $fname"
 git push origin master
+
+
+echo "     "
 echo "--------- Completed update on Git ------"
